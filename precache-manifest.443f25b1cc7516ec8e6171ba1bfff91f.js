5d3740072980a9c91f14fdb48d1e4b844e45a58c
Cole Schneider <colekschneider@gmail.com>
2019-11-09 12:24:45 -0800

    Updates

---
+++ precache-manifest.443f25b1cc7516ec8e6171ba1bfff91f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d58ce8c83443d251cf1390a6a5b741b",
+    "url": "/patent-search/index.html"
+  },
+  {
+    "revision": "6666bc0f8c3b589d7ebb",
+    "url": "/patent-search/static/css/main.6c2a9652.chunk.css"
+  },
+  {
+    "revision": "404f98f7e95bdfedf599",
+    "url": "/patent-search/static/js/2.7e0e81e3.chunk.js"
+  },
+  {
+    "revision": "6666bc0f8c3b589d7ebb",
+    "url": "/patent-search/static/js/main.572d4d7d.chunk.js"
+  },
+  {
+    "revision": "83647493454ef701c36d",
+    "url": "/patent-search/static/js/runtime-main.2a307bde.js"
+  }
+]);
