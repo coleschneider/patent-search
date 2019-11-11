0f95120a98f09eacd93c5e5d9d25603cf8ed19ba
Cole Schneider <colekschneider@gmail.com>
2019-11-10 22:25:41 -0800

    Updates

---
+++ precache-manifest.cbc963d7a8414ebad25ee7900ad0a212.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ab75c6d76dcbc9d419987619c936ab2",
+    "url": "/patent-search/index.html"
+  },
+  {
+    "revision": "b1dc42c02dff0cc2093d",
+    "url": "/patent-search/static/css/main.b8efa882.chunk.css"
+  },
+  {
+    "revision": "15854c414b4a6f1b14c3",
+    "url": "/patent-search/static/js/2.7e53ec67.chunk.js"
+  },
+  {
+    "revision": "b1dc42c02dff0cc2093d",
+    "url": "/patent-search/static/js/main.a7eafa64.chunk.js"
+  },
+  {
+    "revision": "83647493454ef701c36d",
+    "url": "/patent-search/static/js/runtime-main.2a307bde.js"
+  },
+  {
+    "revision": "a47f10ed9bcf471953d12e78a3ba3217",
+    "url": "/patent-search/static/media/LeftArrow.a47f10ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/patent-search/precache-manifest.6825d2d65c50d2d8ce70d9f19a745fc1.js"
+  "/patent-search/precache-manifest.cbc963d7a8414ebad25ee7900ad0a212.js"
 );
 
 self.addEventListener('message', (event) => {
