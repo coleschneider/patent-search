b4e1d04d0d5fcac4a611d76f5be0c7eda712bf93
Cole Schneider <colekschneider@gmail.com>
2019-11-17 10:47:06 -0800

    clean up

--- src/components/ProgressIndicator/ProgressIndicator.tsx
+++ src/components/ProgressIndicator/ProgressIndicator.tsx
@@ -1,47 +1,79 @@
 import React from 'react'
 import { animated } from 'react-spring'
+import styled from 'styled-components'
 
+const Progress_Title = styled.span`
+  font-weight: bold;
+  font-size: 16px;
+`
+const Progress_Description = styled.span`
+  font-size: 14px;
+  margin-top: 4px;
+`
+const Progress_Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 24px;
+  margin-right: 48px;
+`
+const Progress_Wrapper = styled(animated.div)`
+  display: flex;
+  background-color: #2196f3;
+  align-items: center;
+  border-radius: 48px;
+  padding: 4px;
+  box-shadow: 0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.22);
+  color: white;
+  position: fixed;
+  bottom: 10px;
+  right: 5px;
+`
+interface ChartProps {
+  progress: number
+  innerCircleColor: string
+  outterCircleColor: string
+}
 
 const PieChartProgress = ({
-spring,
-progress,
-outterCircleColor = 'white',
-innerCircleColor = 'rgba(33,150,243, 0.6)',
-}) => {
-return (
-  <animated.svg height="60" width="60" viewBox="0 0 24 24">
-    <circle r="12" cx="12" cy="12" fill={outterCircleColor} />
-    <circle
-      r="5"
-      cx="12"
-      cy="12"
-      fill="transparent"
-      stroke={innerCircleColor}
-      strokeWidth="10"
-      strokeDasharray={`calc(${progress || 0} * 31.42 / 100) 31.42`}
-      transform="rotate(-90) translate(-24)"
-      style={{ transition: 'all 0.25s ease-in' }}
-    />
-  </animated.svg>
-)
+  progress,
+  outterCircleColor = 'white',
+  innerCircleColor = 'rgba(33,150,243, 0.6)',
+}: ChartProps) => {
+  return (
+    <animated.svg height="60" width="60" viewBox="0 0 24 24">
+      <circle r="12" cx="12" cy="12" fill={outterCircleColor} />
+      <circle
+        r="5"
+        cx="12"
+        cy="12"
+        fill="transparent"
+        stroke={innerCircleColor}
+        strokeWidth="10"
+        strokeDasharray={`calc(${progress || 0} * 31.42 / 100) 31.42`}
+        transform="rotate(-90) translate(-24)"
+        style={{ transition: 'all 0.25s ease-in' }}
+      />
+    </animated.svg>
+  )
 }
-const ProgressIndicator = ({ spring, active, title, description, progress, className }) => (
-<animated.div
-  style={{
-    transform: spring.x.interpolate(y => {
-      return `translateX(${y}px)`
-    }),
-  }}
-  className="batch-upload-progress__container"
->
-  <React.Fragment>
-    <div className="batch-upload-progress__title-container">
-      <span className="batch-upload-progress__title">{title}</span>
-      <span className="batch-upload-progress__description">{description}</span>
-    </div>
-    <PieChartProgress spring={spring} progress={progress} />
-  </React.Fragment>
-</animated.div>
+interface IndicatorProps extends ChartProps {
+  title: string
+  description: string
+}
+const ProgressIndicator = ({ spring, title, description, progress }: IndicatorProps) => (
+  <Progress_Wrapper
+    style={{
+      transform: spring.x.interpolate(y => {
+        return `translateX(${y}px)`
+      }),
+    }}
+  >
+    <Progress_Container>
+      <Progress_Title>{title}</Progress_Title>
+      <Progress_Description>{description}</Progress_Description>
+    </Progress_Container>
+    <PieChartProgress progress={progress} />
+  </Progress_Wrapper>
 )
 
 export default ProgressIndicator
--- src/utils/download.tsx
+++ src/utils/download.tsx
@@ -1,70 +1,8 @@
-import XLSX from 'xlsx'
 import { saveAs } from 'file-saver'
 import * as _ from 'lodash'
-import { companyPatents, companyPatentsLimit } from './service'
 
-/*
-{
-    "applicationType":"UTILITY",
-    "documentId":"US10001983B2"
-    "applicationNumber":"US15221496"
-    "documentType":"grant"
-    "patentNumber":"10001983"
-    "publicationDate":"2018-06-19T00:00:00Z"
-    "documentDate":"2018-06-19T00:00:00Z"
-    "productionDate":"2018-06-04T00:00:00Z"
-    "applicationDate":"2016-07-27T00:00:00Z"
-    "applicant":["salesforce.com
-     inc."]
-    "inventor":["Roy
-     Saptarshi"
-    "Silver
-     Daniel C."
-    "Schneider
-     Donovan"
-    "Pradhan
-     Medha"
-    "Lamore
-     Ryan"
-    "Rao
-     Naveen Purushothama"
-    "Geh
-     Nicholas"
-    "Koneru
-     Srirama"]
-    "assignee":["salesforce.com
-     Inc."]
-    "title":"Rolling Version Update Deployment Utilizing Dynamic Node Allocation"
-    "archiveUrl":"https:\/\/bulkdata.uspto.gov\/data\/patent\/grant\/redbook\/fulltext\/2018\/ipg180619.zip"
-    "pdfPath":"NOTAVAILABLE"
-    "year":"2018"
-    "_version_":1603727683805511680}
-*/
-
-async function download(buff) {
-  // const wb = XLSX.utils.book_new()
-  // const { data } = await companyPatentsLimit(company, 1)
-
-  // const t1 = performance.now()
-  // console.log(`Call to workPayload took ${t1 - t0} milliseconds.`)
-  // wb.Props = {
-  //   Title: `${company} Patents`,
-  //   Subject: 'Test',
-  //   // Author: `${firstName} ${lastName}`,
-  //   CreatedDate: new Date(2017, 12, 19),
-  // }
-
-  // wb.SheetNames.push('Test Sheet')
-
-  // // const parsedWorkbook = companyPatents.reduce(patent => {
-
-  // // })
-  // // const payload = [{ hello: 'world', hello2: 'world 2' }, { hello: 'world-3', hello2: 'world-4' }]
-
-  // const ws = XLSX.utils.json_to_sheet(workbookPayload)
-  // wb.Sheets['Test Sheet'] = ws
-  // const wbout = XLSX.write(wb, { bookType: 'xlsx', type: 'binary' })
-  function s2ab(s) {
+async function download(buff: string) {
+  function s2ab(s: string) {
     const buf = new ArrayBuffer(s.length)
     const view = new Uint8Array(buf)
     for (let i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xff
